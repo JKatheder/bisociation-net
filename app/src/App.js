3305ca4d21d8374f90f6ba3e0ede9ff760611a7d
Xenia <xenia.rieger24@gmail.com>
2022-05-06 12:04:30 +0200

    include ProjectList on main page (App.js)

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
+import Test from './components/ProjectList.js'
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        title > "Bisociation Net" < /title> <
-        /header> <
-        /div>
+    return (
+        <div className = "App">
+        <Test />
+          <header className = "App-header">
+            <h1>Bisociation Net</h1>
+          </header>
+        </div>
     );
 }
 
--- app/src/components/ProjectList.js
+++ app/src/components/ProjectList.js
@@ -1,3 +1,6 @@
 import './ProjectList.css';
 
 // hier Code für ListView der Projekte, in ProjectList.css Format definieren
+function Test() {
+    return <h2>Project 1</h2>;
+}
