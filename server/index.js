f279e5cdced6e9aef9dbd6c0dce8652a45d49478
Xenia <xenia.rieger24@gmail.com>
2022-05-29 01:18:33 +0200

    allow cross origin data

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
+var cors = require('cors');
 
 // run server on port 3001 unless another port is specified
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// allow our client to see our data
+app.use(cors({ origin: 'http://localhost:3000', methods: 'GET,PUT,POST,OPTIONS', credentials: true }));
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.send("[{text:\"text1 ist viel zu lange daher wird das ersetzt...\", date:\"02.03.22\", title:\"Card 1\"}," +
-        "{text:\"text2\", date:\"02.03.22\", title:\"Card 2: Projekt\"}," +
-        "{text:\"text3\",date: \"02.03.22\",title:\"Card 3\"}," +
-        "{text:\"text4\" date:\"02.04.22\" title:\"Card 4\"}]")
+    res.json({
+        1: { text: "text1 ist viel zu lange daher wird das ersetzt...", date: "02.03.22", title: "Card 1" },
+        2: { text: "text2", date: "02.03.22", title: "Card 2: Projekt" },
+        3: { text: "text3", date: "02.03.22", title: "Card 3" },
+        4: { text: "text4", date: "02.04.22", title: "Card 4" }
+    })
 });
