a02c8d70303ffe5ad64b4a5e1fa6085fcb464c83
Xenia <xenia.rieger24@gmail.com>
2022-06-09 14:32:51 +0200

    New Item finished

--- app/src/components/ProjectList/ProjectList.js
+++ app/src/components/ProjectList/ProjectList.js
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 
-export default function ProjektListe() {
+export default function ProjectList() {
     const [arrayItems, setArrayItems] = useState([]);
 
     useEffect(() => {
@@ -16,16 +16,15 @@ export default function ProjektListe() {
             .get('http://localhost:3001')
             .then((res) => {
                 // saves objects from json in form of Items
-                Object.keys(res.data).forEach(function(key) {
-                    var item = res.data[key];
+                res.data.map((item) =>
                     // TODO: check when database is connected
                     arrayIt.push({
                         content: item.text,
                         date: item.date,
                         title: item.title,
                         id: item.id,
-                    });
-                });
+                    })
+                );
                 setArrayItems(arrayIt);
             })
             .catch((err) => console.log(err));
@@ -33,36 +32,37 @@ export default function ProjektListe() {
 
     //renders Item so that it can be added in return
     const RenderItems = () =>
-        arrayItems.map((item) => ( <
-            Col xl = { 3 } >
-            <
-            Item text = { item.content }
-            date = { item.date }
-            title = { item.title }
-            id = { item.id }
-            onDelete = { setArrayItems }
-            allItems = { arrayItems } >
-            < /Item> <
-            /Col>
+        arrayItems.map((item) => (
+            <div className="col col-lg-3" key={item.id}>
+                <Item
+                    text={item.content}
+                    date={item.date}
+                    title={item.title}
+                    id={item.id}
+                    onDelete={setArrayItems}
+                    allItems={arrayItems}
+                ></Item>{' '}
+            </div>
         ));
 
-    return ( <
-        div >
-        <
-        h1 style = {
-            { padding: 50 } } > My Projects < /h1> <
-        Container >
-        <
-        Row >
-        <
-        Col xs = { 3 } >
-        <
-        NewItem > < /NewItem> <
-        /Col> <
-        RenderItems / >
-        <
-        /Row> <
-        /Container> <
-        /div>
+    // creates Button for new Item
+    const RenderNewItem = () => {
+        return (
+            <NewItem onSave={setArrayItems} prevItems={arrayItems}></NewItem>
+        );
+    };
+
+    return (
+        <div>
+            <h1 style={{ padding: 50 }}> My Projects </h1>{' '}
+            <Container>
+                <Row>
+                    <Col xs={3}>
+                        <RenderNewItem />
+                    </Col>{' '}
+                    <RenderItems />
+                </Row>{' '}
+            </Container>{' '}
+        </div>
     );
 }
