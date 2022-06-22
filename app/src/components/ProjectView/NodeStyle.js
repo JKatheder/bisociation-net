dd9a340b31b73ff01f8c426295c95255e8e6b3d4
Wolke88 <79335375+Wolke8@users.noreply.github.com>
2022-06-22 18:10:37 +0200

    NodeStyle

---
+++ app/src/components/ProjectView/NodeStyle.js
@@ -0,0 +1,10 @@
+import ShapeNodeStyle from 'yfiles';
+
+
+//Style:
+const greenNodeStyle = new ShapeNodeStyle({
+    shape: 'ellipse',
+    fill: 'green',
+    stroke: 'transparent'
+})
+export {greenNodeStyle}
