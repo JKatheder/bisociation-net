6757bc698627b3dc99787594fdc5f793f158d795
Xenia <xenia.rieger24@gmail.com>
2022-06-23 15:35:23 +0200

    add table for nodes

--- server/database.sql
+++ server/database.sql
@@ -4,8 +4,16 @@ CREATE DATABASE bisociationnet;
 \c bisociationnet postgres
 
 CREATE TABLE projects(
-	id           int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	project_id   int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	title        text,
 	date         date,
 	description	 text
+);
+
+CREATE TABLE nodes(
+	node_id      int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	project 	 int REFERENCES projects(project_id),
+	x_pos		 int,
+	y_pos 		 int,
+	content		 text
 );
