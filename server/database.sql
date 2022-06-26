f40519c5b80f7c94be08d82e469035ded7079b7a
Xenia <xenia.rieger24@gmail.com>
2022-06-26 14:52:32 +0200

    correct id names

--- server/database.sql
+++ server/database.sql
@@ -3,8 +3,7 @@ CREATE DATABASE bisociationnet;
 \c bisociationnet
 
 CREATE TABLE projects(
-	node_id      int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	id           int,
+	project_id   int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	title        text,
 	date         date,
 	description	 text
@@ -12,7 +11,7 @@ CREATE TABLE projects(
 
 CREATE TABLE nodes(
 	node_id      int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	project 	 int REFERENCES projects(project_id),
+	project_id 	 int REFERENCES projects(project_id),
 	x_pos		 int,
 	y_pos 		 int,
 	content		 text
