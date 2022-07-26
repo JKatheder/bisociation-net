82c3fe417b80e41960461c85d479e0c50be55581
noemi.weber@student.uni-tuebingen.de <noemi.weber@student.uni-tuebingen.de>
2022-07-26 12:31:34 +0200

    add navbar to projectlist, change button colors

--- app/src/components/ProjectList/ProjectList.js
+++ app/src/components/ProjectList/ProjectList.js
@@ -5,6 +5,9 @@ import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 export default function ProjectList() {
@@ -55,6 +58,16 @@ export default function ProjectList() {
 
     return (
         <div>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        Bisociation Net{' '}
+                    </Navbar.Brand>{' '}
+                    <Form>
+                        <Button variant="secondary">Logout</Button>
+                    </Form>
+                </Container>{' '}
+            </Navbar>{' '}
             <h1 style={{ padding: 50 }}> My Projects </h1>{' '}
             <Container>
                 <Row>
