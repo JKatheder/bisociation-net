af5a5ee0222d6c8a21fef742ecb3ad1575d2037e
NinaM64 <104252092+NinaM64@users.noreply.github.com>
2022-07-26 23:14:07 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
-    password: 'Pos.12.QL',
+    password: '',
     host: 'localhost',
     port: 5432,
     database: 'bisociationnet',
