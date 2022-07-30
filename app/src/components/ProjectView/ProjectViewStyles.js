9654cf1d0d9da2fdabac527adb8def671bc061a9
Wolke88 <79335375+Wolke8@users.noreply.github.com>
2022-07-30 10:37:51 +0200

    CostumizingEdgeColorArrowDesignShiftedInitializeStyles

--- app/src/components/ProjectView/ProjectViewStyles.js
+++ app/src/components/ProjectView/ProjectViewStyles.js
@@ -1,35 +1,61 @@
-import { Fill, ShapeNodeShape,  EdgeStyleDecorationInstaller,
-  NodeStyleDecorationInstaller, ShapeNodeStyle, StyleDecorationZoomPolicy, 
-  Insets, PolylineEdgeStyle } from 'yfiles';
+import { 
+  Fill, 
+  ShapeNodeShape, 
+  EdgeStyleDecorationInstaller, 
+  Size,
+  IArrow,
+  NodeStyleDecorationInstaller,
+  ShapeNodeStyle, 
+  StyleDecorationZoomPolicy, 
+  Insets, 
+  PolylineEdgeStyle } from 'yfiles';
 
 //Colors:
 export const buttonColorDefault = 'rgb(0, 102, 51)'
+export const edgeColorDefault = buttonColorDefault
 export const nodeColorDefault = Fill.DARK_KHAKI
 export const nodeColorStyle1 = 'rgb(99,180,110)'
 export const nodeColorStyle2 = 'rgb(156,76,76)'
 
-//Default Style:
+//Default NodeStyle:
 export const style = new ShapeNodeStyle({
   fill: nodeColorDefault,
-  shape: ShapeNodeShape.ELLIPSE
+  shape: ShapeNodeShape.ELLIPSE,
+  stroke: edgeColorDefault
 })
+
 //Style 1:
 export const greenNodeStyle = new ShapeNodeStyle({
     shape: 'ellipse',
     fill: nodeColorStyle1,
-    stroke: 'black'
+    stroke: edgeColorDefault
   })
+
 //Style 2:
 export const redNodeStyle = new ShapeNodeStyle({
       shape: 'ellipse',
       fill: nodeColorStyle2,
-      stroke: 'black'
+      stroke: edgeColorDefault
 })
+
+//Initialize DefaultStyle eg Nodesize, Edgecolor etc
+export function initializeDefaultStyle(graph, graphComponent){
+  const nodeDefaults = graph.nodeDefaults;
+  nodeDefaults.style = style;
+  graph.nodeDefaults.size = new Size(150, 150);
+
+  //Default EdgesStyle:
+  graph.edgeDefaults.style = new PolylineEdgeStyle({
+    stroke: edgeColorDefault,
+    targetArrow: IArrow.TRIANGLE
+  })
+}
+
 //Decorating the selection of Nodes and Edges
 export function decorateSelection(graphComponent) {
   const graphDecorator = graphComponent.graph.decorator
 //Nodes:
-  const selectionNodeStyle = new ShapeNodeStyle({ shape: 'ellipse', fill: 'none', stroke: '7px solid grey' })
+  const selectionNodeStyle = new ShapeNodeStyle({ shape: 'ellipse', fill: 'none', stroke: '7px solid green' })
   const nodeSelectionDecorator = new NodeStyleDecorationInstaller({
     nodeStyle: selectionNodeStyle,
     zoomPolicy: StyleDecorationZoomPolicy.WORLD_COORDINATES,
@@ -37,7 +63,7 @@ export function decorateSelection(graphComponent) {
   })
   graphDecorator.nodeDecorator.selectionDecorator.setImplementation(nodeSelectionDecorator)
 //Edges:
-  const selectionEdgeStyle = new PolylineEdgeStyle({ stroke: '5px solid grey' })
+  const selectionEdgeStyle = new PolylineEdgeStyle({ stroke: '5px solid green' })
   const edgeSelectionDecorator = new EdgeStyleDecorationInstaller({ edgeStyle: selectionEdgeStyle })
   graphDecorator.edgeDecorator.selectionDecorator.setImplementation(edgeSelectionDecorator)
 }
